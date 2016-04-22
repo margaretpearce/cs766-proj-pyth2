43bc86fe867ee87d66b149e2424f66b94431efee
margaretpearce <margaret.pearce@gmail.com>
2016-04-22 22:48:56 +0000

    Added resize functionality so that the max length of any size is 300px

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -5,6 +5,8 @@ from PIL import Image
 import os
 import uuid
 import time
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -27,9 +29,35 @@ def upload():
     # file upload code here
     if request.method == 'POST':
         file = request.files['file']
-        extension = os.path.splitext(file.filename)[1]
+        image = np.asarray(bytearray(request.files['file'].read()), dtype="uint8")
+
+        # Read to opencv for resizing
+        im = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        height = im.shape[1]
+        width = im.shape[0]
+
+        mult_x = 1
+        mult_y = 1
+
+        # Resize so max length of any side is 300
+        if width > 300:
+            mult_x = 300 / float(width)
+        if height > 300:
+            mult_y = 300 / float(height)
+
+        # Scale
+        if mult_x < mult_y:
+            mult_y = mult_x
+        elif mult_x > mult_y:
+            mult_x = mult_y
+
+        new_height = int(height*mult_y)
+        new_width = int(width*mult_x)
+
+        resized = cv2.resize(im, (new_width, new_height))
 
         # Save as 'original.*' in a unique subdirectory of '/uploads'
+        extension = os.path.splitext(file.filename)[1]
         f_name = str(uuid.uuid4()) + '/original' + extension
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], f_name)
         dir = os.path.dirname(file_path)
@@ -39,7 +67,8 @@ def upload():
             os.makedirs(dir)
 
         # Save the file in this directory
-        file.save(file_path)
+        # file.save(file_path)
+        cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], f_name), resized)
 
         # Save the filename in session and return the filename
         session['imgfile_original'] = f_name
