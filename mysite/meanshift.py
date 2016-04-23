0894e90f653a5d846949fbd2091a674240e0848a
kjha2 <kjha2@wisc.edu>
2016-04-23 14:09:58 -0500

    estimate bandwidth returns too low of a radius so don't use

--- mysite/meanshift.py
+++ mysite/meanshift.py
@@ -7,16 +7,17 @@ import cv2
 def mean_shift(path):
 	image = cv2.imread(path)
 	image_reshape = image.reshape(image.shape[0]*image.shape[1],3)
-	bw = estimate_bandwidth(image_reshape, quantile=0.2, n_samples=500)
-	ms = MeanShift(21, bin_seeding=True)
+	#bw = estimate_bandwidth(image_reshape, quantile=0.2, n_samples=500)
+	bw = 20	
+	ms = MeanShift(bw, bin_seeding=True)
 	ms.fit(image_reshape)
 	labels=ms.labels_
 	cluster_centers = ms.cluster_centers_
 	n_clus = len(np.unique(cluster_centers))
 	#print n_clus
-	quant = ms.cluster_centers_.astype("uint8")[labels]
-	quant = quant.reshape((image.shape[0],image.shape[1],3))
-	plt.imshow(quant)
+	shifted_image = ms.cluster_centers_.astype("uint8")[labels]
+	shifted_image = shifted_image.reshape((image.shape[0],image.shape[1],3))
+	plt.imshow(shifted_image)
 	plt.show()
 	
 mean_shift('baboon.jpg')
