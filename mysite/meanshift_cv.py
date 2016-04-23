625d0439717fd71de978d338417776b10a5c3a4d
kjha2 <kjha2@wisc.edu>
2016-04-23 14:06:47 -0500

    mean shift with opencv library

---
+++ mysite/meanshift_cv.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def meanshift_cv(path):
+	image = cv2.imread(path)
+	spatial_window = 14
+	color_window = 51
+	shifted = cv2.pyrMeanShiftFiltering(image, spatial_window, color_window)
+	plt.imshow(shifted)
+	plt.show()
+
+
+meanshift_cv('baboon.jpg')
+	
