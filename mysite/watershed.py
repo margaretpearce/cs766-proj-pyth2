17e5b77b47616a779e36f6cdce07fb63757b0028
kjha2 <kjha2@wisc.edu>
2016-04-24 14:11:48 -0500

    add canny edge detector

--- mysite/watershed.py
+++ mysite/watershed.py
@@ -6,19 +6,25 @@ def watershed(path):
 	image = cv2.imread(path)
 	gray_image = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
 	ret,thresh = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	foreground = cv2.erode(thresh, None, iterations = 3)
-	background = cv2.dilate(thresh, None, iterations = 3)
+	foreground = cv2.erode(thresh, None, iterations = 1)
+	background = cv2.dilate(thresh, None, iterations = 1)
 	ret,background = cv2.threshold(background,1,128,1)
 	marker = cv2.add(foreground,background)
-
+	canny = cv2.Canny(marker,110,150)
+	contoured_image, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	#plt.imshow(contoured_image)
+	#plt.show()
 	marker32 = np.int32(marker)
 	cv2.watershed(image,marker32)
 	m = cv2.convertScaleAbs(marker32)
 	ret,thresh = cv2.threshold(m,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	thresh_not = cv2.bitwise_not(thresh)
 	res = cv2.bitwise_and(image,image,mask = thresh)
+	res2 = cv2.bitwise_and(image,image,mask=thresh_not)
+	res3 = cv2.addWeighted(res,1,res2,1,0)
+	final = cv2.drawContours(res3, contours, -1, (255, 0, 0), 1)
 
-	return res
-
+	return final
 	#plt.imshow(res)
 	#plt.show()
 
